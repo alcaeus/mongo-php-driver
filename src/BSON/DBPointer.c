abe193fc09177af26f486e2e741348ff3d104eb4
Andreas Braun <git@alcaeus.org>
2021-02-09 11:08:46 +0100

    Fix wrong usage of strncpy
    With GCC >= 8, strncpy triggers warnings when the string would be truncated. Since strncopy fills up with a null byte, we can copy the 24 characters of the ID and letl strncpy copy the null byte.

--- src/BSON/DBPointer.c
+++ src/BSON/DBPointer.c
@@ -46,7 +46,7 @@ static bool php_phongo_dbpointer_init(php_phongo_dbpointer_t* intern, const char
 	intern->ref     = estrndup(ref, ref_len);
 	intern->ref_len = ref_len;
 
-	strncpy(intern->id, id, sizeof(intern->id));
+	strncpy(intern->id, id, sizeof(intern->id) - 1);
 
 	return true;
 } /* }}} */
